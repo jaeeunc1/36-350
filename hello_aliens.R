c8954a741f142a5769473e9062fd846dc5b83796
Jenn <jaeeunc1@andrew.cmu.edu>
2017-12-10 16:58:50 -0500

    35-360 final project

---
+++ hello_aliens.R
@@ -0,0 +1,10 @@
+
+cat("Hello aliens, \n 
+  My name is Jenn. I am sending you guys this message without the intent to harm to confuse you guys. \n 
+I just want to get to know you guys and become friends. Well, I have been writing some statistical operations and \n
+I realized that there is no planet like earth. So How do you guys manage to live in other planets? What makes you guys
+so special and different from us? \n
+  If you can read this message, please reply to me using ur alien powers.\n
+
+Peace. \n
+Love, Jenn \n ")
